29fe9e42e417d821d0c06efaa47f47b628705cf1
Israel2800 <dady281100@gmail.com>
2025-01-04 15:57:37 -0600

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.israelaguilar.prohiringandroid"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.israelaguilar.prohiringandroid"
